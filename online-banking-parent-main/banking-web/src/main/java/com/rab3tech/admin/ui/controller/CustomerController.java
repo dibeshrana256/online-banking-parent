391f6647527a2bff13ebc38ae5be85529c38fc0d
DIBESHPC\broad <dibeshrana.dr@gmail.com>
2024-12-22 15:05:10 -0600

    no message

---
+++ online-banking-parent-main/banking-web/src/main/java/com/rab3tech/admin/ui/controller/CustomerController.java
@@ -0,0 +1,98 @@
+package com.rab3tech.admin.ui.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.rab3tech.customer.service.CustomerService;
+import com.rab3tech.vo.CustomerUpdateVO;
+import com.rab3tech.vo.CustomerVO;
+import com.rab3tech.vo.RoleVO;
+
+@Controller
+@RequestMapping("/admin")
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/customers")
+	public String showCustomer(Model model) {
+	   List<CustomerVO> customerVOs=customerService.findCustomers();
+	   model.addAttribute("customerVOs", customerVOs);
+	   return "admin/customers";
+	}
+		
+	
+	@GetMapping("/customers/photo")
+	public void findCustomerPhoto(@RequestParam int cid,HttpServletResponse response) throws IOException {
+	   byte[] photo=customerService.findPhotoByid(cid);
+	   response.setContentType("image/png");
+	   ServletOutputStream outputStream=response.getOutputStream();
+	   if(photo!=null) {
+		   outputStream.write(photo);
+	   }else {
+		   outputStream.write(new byte[] {});
+	   }
+	   outputStream.flush();
+	   outputStream.close();
+	}
+	
+	/*@PostMapping("/updateCustomer")
+	public String updateCustomer(@RequestParam("cid") int cid,@RequestParam String name,String mobile,MultipartFile photo,Model model) throws IOException {
+		CustomerVO customerVO=new CustomerVO();
+		customerVO.setId(cid);
+		customerVO.setName(name);
+		customerVO.setMobile(mobile);
+		customerVO.setImage(photo.getBytes());
+		customerService.updateProfile(customerVO);
+		//redirect to show all the records of the current customer in the database!
+	   return "admin/customers";
+	}*/
+	
+	@PostMapping("/updateCustomer")
+	public String updateCustomer(@ModelAttribute CustomerUpdateVO customerUpdateVO,Model model) throws IOException {
+		customerService.updateProfile(customerUpdateVO);
+		//redirect to show all the records of the current customer in the database!
+	   return "redirect:/admin/customers";
+	}
+	
+	@GetMapping("/filter/customers")
+	public String showCustomerByFilter(@RequestParam String filterDropdown, Model model) {
+		List<CustomerVO> filteredList = customerService.filterCustomerByRole(filterDropdown);
+		
+		List<RoleVO> roleVOs = customerService.getRoles();
+		model.addAttribute("roleVOs",roleVOs);
+		
+		model.addAttribute("customerVOs",filteredList);
+		model.addAttribute("filterDropdown",filterDropdown);
+		
+		return "admin/dashboard";
+	}
+	
+	@GetMapping("/sortBy/name/{filterDropdown}")
+	public String sortByName(@PathVariable ("filterDropdown") String filterDropdown, Model model) {
+		List<CustomerVO> filteredList = customerService.filterCustomerByRole(filterDropdown);
+		List<CustomerVO> sortedCustomers = customerService.sortCustomersByNameASC(filteredList);
+		model.addAttribute("customerVOs", sortedCustomers);
+		
+		List<RoleVO> roleVOs = customerService.getRoles();
+		model.addAttribute("roleVOs",roleVOs);
+		return "admin/dashboard";
+	}
+}
